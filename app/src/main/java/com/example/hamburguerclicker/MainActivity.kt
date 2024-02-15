9d4890489bcbd873d9c271e247c11ae92ae586ed
Andres <andresgalindoescalante@gmail.com>
2024-02-15 10:07:03 +0100

    0.0.1

--- app/src/main/java/com/example/hamburguerclicker/MainActivity.kt
+++ app/src/main/java/com/example/hamburguerclicker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.hamburguerclicker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -11,13 +14,37 @@ import com.example.hamburguerclicker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+     lateinit var binding: ActivityMainBinding
+     lateinit var txtValorPeso: TextView
+     lateinit var unidad: TextView
+     lateinit var hamburguesa:ImageView
+     lateinit var btnRestar:Button
+
+    var pesoTotal=900.0
+    var unidadPeso="Mili Gramos"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        txtValorPeso=findViewById(R.id.txtValorPeso)
+        hamburguesa=findViewById(R.id.hamburguesa)
+        unidad=findViewById(R.id.unidadPeso)
+        btnRestar=findViewById(R.id.restar)
+
+        hamburguesa.setOnClickListener(){
+            pesoTotal+=100
+            txtValorPeso.setText(""+pesoTotal)
+            unidad()
+        }
+
+        btnRestar.setOnClickListener(){
+            pesoTotal-=100
+            txtValorPeso.setText(""+pesoTotal)
+            unidad()
+        }
 
         val navView: BottomNavigationView = binding.navView
 
@@ -31,5 +58,33 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun unidad(){
+
+        when(unidadPeso){
+            "Mili Gramos" ->{
+                if(pesoTotal>=1000){
+                    unidadPeso="Gramos"
+                    pesoTotal=pesoTotal/1000
+                    unidad.setText(unidadPeso)
+                    txtValorPeso.setText(""+pesoTotal)
+                }
+            }
+            "Gramos"->{
+                if(pesoTotal>=1000){
+                    unidadPeso="Kilos"
+                    pesoTotal=pesoTotal/1000
+                    unidad.setText(unidadPeso)
+                    txtValorPeso.setText(""+pesoTotal)
+                }else if(pesoTotal<1){
+                    unidadPeso="Mili Gramos"
+                    pesoTotal=pesoTotal*-10
+                    unidad.setText(unidadPeso)
+                    txtValorPeso.setText(""+pesoTotal)
+                }
+            }
+        }
+
     }
 }
