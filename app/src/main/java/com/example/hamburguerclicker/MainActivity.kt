b3575afc4aac4729a101cb491486b7051fc7545a
javier <javipc14@gmail.com>
2024-02-19 11:56:49 +0100

    0.0.5

--- app/src/main/java/com/example/hamburguerclicker/MainActivity.kt
+++ app/src/main/java/com/example/hamburguerclicker/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
      lateinit var btnRestar:Button
      lateinit var btnCompraCarne:Button
 
-//aa
+//aaa
     var pesoTotal=900.0
     var unidadPeso="Mili Gramos"
 
